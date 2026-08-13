46b6794b4e9d039aa392ade249298b719701dbb4
karin-nhs <karin.kunrath@nhs.net>
2026-08-13 14:32:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CG_CONFIG = {
-  supabaseUrl: "https://iyzyjgdxtvzuuywlumaa.supabase.co",
+  supabaseUrl: "https://iyzylgdxtvzuuywlumaa.supabase.co",
   supabaseKey: "sb_publishable_ilqNDO1PMp_LvSH8dPaXVw_jGIRmDyy"
 };
